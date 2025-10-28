045d85dbcc3e60ff1705fa56a783ea3d5785a99d
LearnwithPeter <learnwithpeter543@gmail.com>
2025-10-28 10:56:56 +0530

    app structure

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import Whychooseus from '@/components/Whychooseus'
+import { InfiniteMovingCardsDemo } from '@/components/InfiniteMovingCardsDemo'
+function page() {
+  return (
+    <div>
+      <Navbar/>
+
+      <InfiniteMovingCardsDemo/>
+
+    </div>
+  )
+}
+
+export default page
