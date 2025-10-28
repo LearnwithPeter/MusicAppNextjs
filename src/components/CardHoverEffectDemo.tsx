6e0f7ea47b2bee9c40dfd703a84e8be5c72fa01c
LearnwithPeter <learnwithpeter543@gmail.com>
2025-10-28 10:58:49 +0530

    card hover effect

---
+++ src/components/CardHoverEffectDemo.tsx
@@ -0,0 +1,9 @@
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+ 
+export function CardHoverEffectDemo() {
+  return (
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={} />
+    </div>
+  );
+}
