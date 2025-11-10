9c9cd25dd1cd2736f9a07da8b67e11e60dca8e78
LearnwithPeter <learnwithpeter543@gmail.com>
2025-11-10 11:45:10 +0530

    contact-us

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {MeteorsDemo} from "@/components/MeteorsDemo"
+function page() {
+  return (
+    <div>
+      <MeteorsDemo/>
+    </div>
+  )
+}
+
+export default page
